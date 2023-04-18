b5ec7a6c6b6d96cc62f0a347e6e9eebe61429d3e
walternomas <w.barriosvazquez@gmail.com>
2023-04-18 12:30:20 -0300

    primera prueba de rutas

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+/**
+ * Route:get    | Consultar
+ * Route:post   | Guardar
+ * Route:delete | Eliminar
+ * Route:put    | Actualizar
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ruta home';
+});
+
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    // consultar la base de datos
+
+    return $slug;
 });
