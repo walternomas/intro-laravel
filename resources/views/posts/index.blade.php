79780cc0e6e1e01470051653660d287cc62fdb68
walternomas <w.barriosvazquez@gmail.com>
2023-04-29 10:43:58 -0300

    primer paso en la creación y edición de posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2
+            class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight flex items-center justify-between">
             {{ __('Posts') }}
+            <a href="{{ route('posts.create') }}" class="text-xs bg-red-800 text-white rounded px-2 py-1">Crear</a>
         </h2>
     </x-slot>
 
@@ -15,7 +17,7 @@
                             <tr class="border-b border-gray-200 text-sm">
                                 <td class="px-6 py-4">{{ $post->title }}</td>
                                 <td class="px-6 py-4">
-                                    <a href="" class="text-indigo-600">Editar</a>
+                                    <a href="{{ route('posts.edit', $post) }}" class="text-indigo-600">Editar</a>
                                 </td>
                                 <td class="px-6 py-4">
                                     <form action="{{ route('posts.destroy', $post) }}" method="POST">
@@ -23,7 +25,7 @@
                                         @method('DELETE')
 
                                         <input type="submit" value="Eliminar"
-                                            class="bg-gray-800 text-white rounded px-4 py-2"
+                                            class="bg-red-800 text-white rounded px-4 py-2"
                                             onclick="return confirm('¿Desea eliminar?')">
                                     </form>
                                 </td>
