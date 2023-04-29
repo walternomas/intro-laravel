41aa743b4a03cdb1926366bf9f20a0097ab3c46f
walternomas <w.barriosvazquez@gmail.com>
2023-04-29 10:55:42 -0300

    controles de un formulario

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,1 +1,12 @@
-Formulario
+@csrf
+
+<label for="title" class="uppercase text-gray-700 text-xs">Título</label>
+<input type="text" name="title" class="rounded border-gray-200 w-full mb-4" value="{{ $post->title }}">
+
+<label for="body" class="uppercase text-gray-700 text-xs">Contenido</label>
+<textarea name="body" rows="5" class="rounded border-gray-200 w-full mb-4">{{ $post->body }}</textarea>
+
+<div class="flex justify-between items-center">
+    <a href="{{ route('posts.index') }}" class="text-indigo-600">Volver</a>
+    <input type="submit" value="Enviar" class="bg-red-800 text-white rounded px-4 py-2">
+</div>
