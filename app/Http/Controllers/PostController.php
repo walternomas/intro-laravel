ab25a9c01305f3ca801f1b3cd9c59b511a30b7c9
walternomas <w.barriosvazquez@gmail.com>
2023-04-29 10:02:00 -0300

    eliminar un post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,4 +13,10 @@ class PostController extends Controller
             'posts' => Post::latest()->paginate()
         ]);
     }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return back();
+    }
 }
