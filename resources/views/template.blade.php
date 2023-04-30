b64e7ce0090757aa015675b0657c44b4f5752ead
walternomas <w.barriosvazquez@gmail.com>
2023-04-30 10:16:42 -0300

    diseño personalizado

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -27,7 +27,21 @@
             @endauth
         </header>
 
+        <div class="opacity-60 h-px mb-8"
+            style="
+        background: linear-gradient(to right,
+            rgba(200, 200, 200, 0) 0%,
+            rgba(200, 200, 200, 1) 30%,
+            rgba(200, 200, 200, 1) 70%,
+            rgba(200, 200, 200, 0) 100%
+        );">
+
+        </div>
+
         @yield('content')
+        <p class="py-16">
+            <img src="{{ asset('images/logo.png') }}" alt="Elefante" class="h-12 mx-auto">
+        </p>
     </div>
 </body>
 
