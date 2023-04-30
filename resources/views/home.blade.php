67c565a74c1f4caff02cf3588d3d99d464e2fd25
walternomas <w.barriosvazquez@gmail.com>
2023-04-30 10:02:23 -0300

    diseño web

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('template')
 
 @section('content')
-    <h1>Home</h1>
+    <h1>Inicio</h1>
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,23 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Proyecto web</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    <p>
-        <a href="{{ route('home') }}">Home</a>
-        <a href="{{ route('blog') }}">Blog</a>
 
-        @auth
-        <a href="{{ route('dashboard') }}">Dashboard</a>
-        @else
-        <a href="{{ route('login') }}">Login</a>
-        @endauth
-    </p>
-    <hr>
-    @yield('content')
+<body>
+    <div class="container px-4 mx-auto">
+        <header class="flex justify-between items-center py-4">
+            <div class="flex items-center flex-grow gap-4">
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('images/logo.png') }}" alt="Elefante" class="h-12">
+                </a>
+                <form action="">
+                    <input type="text" placeholder="Buscar">
+                </form>
+            </div>
+            @auth
+                <a href="{{ route('dashboard') }}">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
+            @endauth
+        </header>
+
+        @yield('content')
+    </div>
 </body>
+
 </html>
